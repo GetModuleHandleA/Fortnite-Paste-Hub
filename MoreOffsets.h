b623a42989613b9804d9002e91201cfcccda0e66
0x90 <76507977+LordGlizzy@users.noreply.github.com>
2022-05-09 16:55:31 -0700

    Create MoreOffsets.h

---
+++ MoreOffsets.h
@@ -0,0 +1,17 @@
+#include <Windows.h>
+#define uworld = 0xc640158
+namespace deluxontopdaddy {
+	uintptr_t persistentlvl = 0x30;
+	uintptr_t gameinst = 0x1A8; //gameinstance
+	uintptr_t niggerplrs = 0x38; //localplayers (fuck niggers)
+	uintptr_t lpc = 0x30; //localplayercontroller
+	uintptr_t pcm = 0x328; //player-camera-manager
+	uintptr_t localp = 0x320; //localpawn
+	uintptr_t rootcomp = 0x188; //rootcomponent
+	uintptr_t ps = 0x290; //playerstate
+	uintptr_t lt = 0x1020; //localteam
+	uintptr_t ac = 0xA0; //actorcount
+	uintptr_t actors = 0x98; //aactors
+}
+
+//stole this shit from https://github.com/skidofficial/fortnite-offsets/blob/main/offsets.h
